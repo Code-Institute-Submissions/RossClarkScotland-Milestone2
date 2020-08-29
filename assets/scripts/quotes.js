8bec2c7ec9acce6fe0a34c85c4c9631b17e08e77
RossClarkScotland <rossclarkscotland@gmail.com>
2020-08-29 18:24:40 +0000

    Added quote generator content

---
+++ assets/scripts/quotes.js
@@ -0,0 +1,25 @@
+console.log("Kimba!");
+
+let quotes = [
+    "Organizing gods is like herding cats into straight lines. They don't take naturally to it.",
+    "Angry gets shit done.",
+    "Every hour wounds. The last one kills",
+    "Even nothing cannot last forever",
+    "He wondered whether home was a thing that happened to a place after a while, or if it was something that you found in the end, if you simply walked and waited and willed it long enough.",
+    "As sure as water's wet and days are long and a friend will always disappoint you in the end.",
+    "I think I would rather be a man than a god. We don't need anyone to believe in us. We just keep going anyhow. It's what we do.",
+    "He was no longer scared of what tommorow might bring because yesterday had brought it.",
+    "You are an analog girl, living in a digital world.",
+    "People populate the darkness; with ghosts, with gods, with electrons, with tales.",
+    "I believe that anyone who claims to know what's going on will lie about the little things too.",
+    "If hell is other people . . . then ppurgatory is airports.",
+    "There was nowhere they could have gone and they went there anyway.",
+    "Information and knowledge: to currencies that have never gone out of style.",
+    "Mostly you are what you think you are."
+
+];
+
+function quoteGenerator() {
+    let randomNum = Math.floor(Math.random() * (quotes.length));
+    document.getElementById('quoteScreen').innerHTML = quotes[randomNum];
+}
