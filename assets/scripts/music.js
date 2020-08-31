114728b41b6d0d431bfb62e8f5314e751f4b176c
RossClarkScotland <rossclarkscotland@gmail.com>
2020-08-31 12:19:19 +0000

    Styled masic page, restyled nav bar and banner image

---
+++ assets/scripts/music.js
@@ -0,0 +1,210 @@
+
+    function player() {
+        let rando = Math.random();
+        let num = (rando * 52) + 1;
+        let playlist = Math.floor(num);
+
+        if (playlist == 1) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/I5emPqmZSrM");
+        }
+
+        if (playlist == 2) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/HwOWX6b8HHw");
+        }
+
+        if (playlist == 3) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/OuC519ni1aE");
+        }
+
+         if (playlist == 4) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/DgGuTicdtyg");
+        }
+
+        if (playlist == 5) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/sGDuGybCRSE");
+        }
+
+        if (playlist == 6) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/W4euyTDhFnk");
+        }
+
+        if (playlist == 7) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/kEGuHdKn0Lc");
+        }
+
+        if (playlist == 8) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/Bi6YHWjy3uU");
+        }
+
+        if (playlist == 9) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/T5al0HmR4to");
+        }
+
+        if (playlist == 10) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/SLCuL-K39eQ");
+        }
+
+        if (playlist == 11) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/DE5lsoXeQDg");
+        }
+
+        if (playlist == 12) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/euJ22UqLD5Y");
+        }
+
+        if (playlist == 13) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/mrEk06XXaAw");
+        }
+
+        if (playlist == 14) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/lot1xjsFbTc");
+        }
+
+        if (playlist == 15) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/De1LCQvbqV4");
+        }
+
+        if (playlist == 17) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/JL5sv1_1MQM");
+        }
+
+        if (playlist == 18) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/aG-kLDAj8mc");
+        }
+
+        if (playlist == 19) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/uhRjyLLUdhw");
+        }
+
+        if (playlist == 20) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/CsZ4XIOEKLY");
+        }
+
+        if (playlist == 21) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/Z9GbGO7CKdQ");
+        }
+
+        if (playlist == 22) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/sh5gtGk9XZ8");
+        }
+
+        if (playlist == 23) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/98JrWm9IMMA");
+        }
+
+        if (playlist == 24) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/FSZxJpf1IHI");
+        }
+
+        if (playlist == 25) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/2mhPjXiZg64");
+        }
+
+        if (playlist == 26) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/2Q_ZzBGPdqE");
+        }
+
+        if (playlist == 27) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/OtTmR_v6pz4");
+        }
+
+         if (playlist == 28) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/9MO1KyNUOIs");
+        }
+        
+          if (playlist == 29) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/UCrSmJ_YCk8");
+        }
+
+          if (playlist == 30) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/bch1_Ep5M1s");
+        }
+
+          if (playlist == 31) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/9xFkjmYaAfQ");
+        }
+
+          if (playlist == 32) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/hX8RXWBXSXM");
+        }
+
+          if (playlist ==33) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/hztBex4Ebgg");
+        }
+
+          if (playlist == 34) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/dVxYL2sI1oM");
+        }
+
+          if (playlist == 35) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/V_IrdS-zu48");
+        }
+
+          if (playlist == 36) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/zrcpOncueZI");
+        }
+
+          if (playlist == 37) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/6p-lDYPR2P8");
+        }
+
+          if (playlist == 38) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/l5FyEQ7cqrM");
+        }
+
+          if (playlist == 39) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/o7U3lo80YrQ");
+        }
+
+          if (playlist == 40) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/VXRQV5z9GL8");
+        }
+
+        if (playlist == 41) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/0lAK9trqgAg");
+        }
+
+        if (playlist == 42) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/B3oCB9RdiB4");
+        }
+
+        if (playlist == 43) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/T_Cqy3uxRXc");
+        }
+
+        if (playlist == 44) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/yJXN5VPkyH8");
+        }
+
+        if (playlist == 45) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/sYraVHUONeQ");
+        }
+
+        if (playlist == 46) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/6ouJPYL9tqI");
+        }
+
+        if (playlist == 47) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/7OUV78aVCXo");
+        }
+
+        if (playlist == 48) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/w4flBb-LbNs");
+        }
+
+        if (playlist == 49) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/qQvIAs-nPSo");
+        }
+
+        if (playlist == 50) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/Rs2nH9gSXOE");
+        }
+
+        if (playlist == 51) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/7Tb1kkg8e5g");
+        }
+
+        if (playlist == 52) {
+            document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/atkVaKz9r5Y");
+        }
+    }
