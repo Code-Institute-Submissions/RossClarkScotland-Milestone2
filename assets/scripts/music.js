50554219a9e57c5f21864e50cdfa761f463e3ae2
RossClarkScotland <rossclarkscotland@gmail.com>
2020-09-02 16:55:45 +0000

    Added song titles to player function

--- assets/scripts/music.js
+++ assets/scripts/music.js
@@ -6,205 +6,256 @@
 
         if (playlist == 1) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/I5emPqmZSrM");
+            document.getElementById("song").innerHTML = "Nottamun Town by Fairport Convention";
         }
 
         if (playlist == 2) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/HwOWX6b8HHw");
+            document.getElementById("song").innerHTML = "Walkin' after midnight by Patsy Cline";
         }
 
         if (playlist == 3) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/OuC519ni1aE");
+            document.getElementById("song").innerHTML = "";
         }
 
          if (playlist == 4) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/DgGuTicdtyg");
+            document.getElementById("song").innerHTML = "Iko Iko by The Dixie Cups";
         }
 
         if (playlist == 5) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/sGDuGybCRSE");
+            document.getElementById("song").innerHTML = "The fool on the hill by The Beatles";
         }
 
         if (playlist == 6) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/W4euyTDhFnk");
+            document.getElementById("song").innerHTML = "Heartbreak Hotel by Elvis Presley";
         }
 
         if (playlist == 7) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/kEGuHdKn0Lc");
+            document.getElementById("song").innerHTML = "Immigrant Song by Led Zepplin";
         }
 
         if (playlist == 8) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/Bi6YHWjy3uU");
+            document.getElementById("song").innerHTML = "Midnight Special by Leadbelly";
         }
 
         if (playlist == 9) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/T5al0HmR4to");
+            document.getElementById("song").innerHTML = "A Hard Rain's Gonna Fall by Bob Dylan";
         }
 
         if (playlist == 10) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/SLCuL-K39eQ");
+            document.getElementById("song").innerHTML = "Night on Bald Mountain - from Fantasia";
         }
 
         if (playlist == 11) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/DE5lsoXeQDg");
+            document.getElementById("song").innerHTML = "";
         }
 
         if (playlist == 12) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/euJ22UqLD5Y");
+            document.getElementById("song").innerHTML = "I have the moon by Magnetic Fields";
         }
 
         if (playlist == 13) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/mrEk06XXaAw");
+            document.getElementById("song").innerHTML = "Ravel's Bolero";
         }
 
         if (playlist == 14) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/lot1xjsFbTc");
+            document.getElementById("song").innerHTML = "";
         }
 
         if (playlist == 15) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/De1LCQvbqV4");
+            document.getElementById("song").innerHTML = "Danse macabre by Bela Fleck";
         }
 
         if (playlist == 17) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/JL5sv1_1MQM");
+            document.getElementById("song").innerHTML = "The blue Danube";
         }
 
         if (playlist == 18) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/aG-kLDAj8mc");
+            document.getElementById("song").innerHTML = "The Emperor Waltz";
         }
 
         if (playlist == 19) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/uhRjyLLUdhw");
+            document.getElementById("song").innerHTML = "Don't fear the reaper by Blue Oyster Cult";
         }
 
         if (playlist == 20) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/CsZ4XIOEKLY");
+            document.getElementById("song").innerHTML = "T.V. Eye by The Stooges";
         }
 
         if (playlist == 21) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/Z9GbGO7CKdQ");
+            document.getElementById("song").innerHTML = "Cat people by David Bowie";
         }
 
         if (playlist == 22) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/sh5gtGk9XZ8");
+            document.getElementById("song").innerHTML = "Sally MacLennane by The Pogues";
         }
 
         if (playlist == 23) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/98JrWm9IMMA");
+            document.getElementById("song").innerHTML = "Little drummer boy by Grace Jones";
         }
 
         if (playlist == 24) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/FSZxJpf1IHI");
+            document.getElementById("song").innerHTML = "";
         }
 
         if (playlist == 25) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/2mhPjXiZg64");
+            document.getElementById("song").innerHTML = "Tango till they're sore by Tom Waits";
         }
 
         if (playlist == 26) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/2Q_ZzBGPdqE");
+            document.getElementById("song").innerHTML = "Help! by The Beatles";
         }
 
         if (playlist == 27) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/OtTmR_v6pz4");
+            document.getElementById("song").innerHTML = "One last hope from Hercules";
         }
 
          if (playlist == 28) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/9MO1KyNUOIs");
+            document.getElementById("song").innerHTML = "Viva Las Vegas by Elvis Presley";
         }
         
           if (playlist == 29) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/UCrSmJ_YCk8");
+            document.getElementById("song").innerHTML = "Why can't he be you by Patsy Cline";
         }
 
           if (playlist == 30) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/bch1_Ep5M1s");
+            document.getElementById("song").innerHTML = "San Francisco by Scott Mckenzie";
         }
 
           if (playlist == 31) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/9xFkjmYaAfQ");
+            document.getElementById("song").innerHTML = "Marie Laveau by Dr Hook";
         }
 
           if (playlist == 32) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/hX8RXWBXSXM");
+            document.getElementById("song").innerHTML = "Marie Laveau by Oscar Celestin";
         }
 
           if (playlist ==33) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/hztBex4Ebgg");
+            document.getElementById("song").innerHTML = "Litanie des saints Dr John";
         }
 
           if (playlist == 34) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/dVxYL2sI1oM");
+            document.getElementById("song").innerHTML = "The lament of the Cherokke by Indian Reservation";
         }
 
           if (playlist == 35) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/V_IrdS-zu48");
+            document.getElementById("song").innerHTML = "Johnyy Appleseed";
         }
 
           if (playlist == 36) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/zrcpOncueZI");
+            document.getElementById("song").innerHTML = "Dark am I yet lovely by Sinead O'Connor";
         }
 
           if (playlist == 37) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/6p-lDYPR2P8");
+            document.getElementById("song").innerHTML = "Material girl by Madonna";
         }
 
           if (playlist == 38) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/l5FyEQ7cqrM");
+            document.getElementById("song").innerHTML = "Old friends";
         }
 
           if (playlist == 39) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/o7U3lo80YrQ");
+            document.getElementById("song").innerHTML = "Cheers theme sons";
         }
 
           if (playlist == 40) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/VXRQV5z9GL8");
+            document.getElementById("song").innerHTML = "Life is a cabaret from Cabaret";
         }
 
         if (playlist == 41) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/0lAK9trqgAg");
+            document.getElementById("song").innerHTML = "In the dark with you by Greg Brown";
         }
 
         if (playlist == 42) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/B3oCB9RdiB4");
+            document.getElementById("song").innerHTML = "Magic bus by The Who";
         }
 
         if (playlist == 43) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/T_Cqy3uxRXc");
+            document.getElementById("song").innerHTML = "Hang me, oh hang me by Dave van Kronk";
         }
 
         if (playlist == 44) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/yJXN5VPkyH8");
+            document.getElementById("song").innerHTML = "Death is not the end by Nick Cave and Friends";
         }
 
         if (playlist == 45) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/sYraVHUONeQ");
+            document.getElementById("song").innerHTML = "Cold wind to Valhalla by Jethro Tull";
         }
 
         if (playlist == 46) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/6ouJPYL9tqI");
+            document.getElementById("song").innerHTML = "Sam Bass by Alan Lomax";
         }
 
         if (playlist == 47) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/7OUV78aVCXo");
+            document.getElementById("song").innerHTML = "Quiet riot by Thunderbird";
         }
 
         if (playlist == 48) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/w4flBb-LbNs");
+            document.getElementById("song").innerHTML = "City of dreams by Talking Heads";
         }
 
         if (playlist == 49) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/qQvIAs-nPSo");
+            document.getElementById("song").innerHTML = "What's new pussycat? by Tom Jones";
         }
 
         if (playlist == 50) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/Rs2nH9gSXOE");
+            document.getElementById("song").innerHTML = "The way you look tonight";
         }
 
         if (playlist == 51) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/7Tb1kkg8e5g");
+            document.getElementById("song").innerHTML = "Don't let me be misunderstood by The Animals";
         }
 
         if (playlist == 52) {
             document.getElementById("jukebox").setAttribute("src", "https://www.youtube.com/embed/atkVaKz9r5Y");
+            document.getElementById("song").innerHTML = "Closer to fine by The Indigo Girls";
         }
     }
