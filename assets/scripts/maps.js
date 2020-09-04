7dc94210362fa6acbf8db801d41df22e466f7ee8
RossClarkScotland <rossclarkscotland@gmail.com>
2020-09-04 21:57:33 +0000

    Fixed map nodes and added page intro

--- assets/scripts/maps.js
+++ assets/scripts/maps.js
@@ -1,20 +1,20 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4.75,
+        zoom: 3.8,
         center: {
-            lat: 39.0997,
-            lng: -94.5786
+            lat: 36.6437,
+            lng: -93.2185
         }
     });
 
     var labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
 
     var locations = [
-        { lat: 38.751365, lng: -90.249011 }, 
-        { lat: 41.675603, lng: -86.078059 },  
-        { lat: 41.881832, lng: -87.623177 },  
-        { lat: 43.0731, lng: -89.4012 },  
-        { lat: 43.099573, lng: -90.135281 },  
+        { lat: 38.6270, lng: -90.1994 }, 
+        { lat: 41.3275, lng: -85.6778 },  
+        { lat: 41.8781, lng: -87.6298 },  
+        { lat: 43.0186, lng: -89.5498 },  
+        { lat: 43.0994, lng: -90.1353 },  
         { lat: 41.3275, lng: -89.1290 },  
         { lat: 40.7392, lng: -89.0165 }, 
         { lat: 37.9137, lng: -89.8221 }, 
@@ -27,14 +27,14 @@ function initMap() {
         { lat: 43.5460, lng: -96.7313 },  
         { lat: 44.9537, lng: -93.0900 },  
         { lat: 40.0150, lng: -105.2705 },  
+        { lat: 37.7945, lng: -93.5780 },
         { lat: 44.9778, lng: -93.2650 },  
-        { lat: 37.7945, lng: -93.5780 },  
         { lat: 39.8097, lng: -98.5556 },  
         { lat: 40.4008, lng: -93.5805 },  
         { lat: 37.2296, lng: -80.4139 },  
         { lat: 34.9734, lng: -85.3502 },  
         { lat: 27.4467, lng: -80.3256 },  
-        { lat: 38.9041, lng: -89.9253 }
+        { lat: 38.9041, lng: -89.9253 }  
     ];
 
     var markers = locations.map(function(location, i) {
