d3c36a99458cdbb0aa4e6eae9aeb3cbed4fe2cd3
RossClarkScotland <rossclarkscotland@gmail.com>
2020-09-05 15:08:20 +0000

    Added sights button to map embed

--- assets/scripts/mapEmbeds.js
+++ assets/scripts/mapEmbeds.js
@@ -6,5 +6,5 @@
             document.getElementById("StLouis").setAttribute("src", "https://www.google.com/maps/embed/v1/search?key=AIzaSyBQrF8tTzmRfEHbHjOpEFQZoUZ16qgrPlg&q=hotels+in+St+Louis");
         }
  function SLSights() {
-            document.getElementById("StLouis").setAttribute("src", "https://www.google.com/maps/embed/v1/search?key=AIzaSyBQrF8tTzmRfEHbHjOpEFQZoUZ16qgrPlg&q=sights+in+St+Louis");
+            document.getElementById("StLouis").setAttribute("src", "https://www.google.com/maps/embed/v1/search?key=AIzaSyBQrF8tTzmRfEHbHjOpEFQZoUZ16qgrPlg&q=tourist+attractions+in+St+Louis");
         }
