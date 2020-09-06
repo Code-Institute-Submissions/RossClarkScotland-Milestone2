2892617dcd974c674a072d5c7e5771db12b8f8dc
RossClarkScotland <rossclarkscotland@gmail.com>
2020-09-06 18:00:54 +0000

    Added The House on the Rock, Peru, El Paso and Chester to Trip page

--- assets/scripts/mapEmbeds.js
+++ assets/scripts/mapEmbeds.js
@@ -58,5 +58,65 @@ MadWIHotels.addEventListener("click", function() {
             document.getElementById("MadWI").setAttribute("src", "https://www.google.com/maps/embed/v1/search?key=AIzaSyDGCPoH-xwhnwXsd7kMVchttZMdFIIyiu8&q=hotels+in+Madison+WI");
         });
 
+var rockDine = document.getElementById("rockDine");
+ rockDine.addEventListener("click", function() {
+     document.getElementById("rock").setAttribute("src", "https://www.google.com/maps/embed/v1/search?key=AIzaSyDGCPoH-xwhnwXsd7kMVchttZMdFIIyiu8&q=restaurants+near+The+House+on+the+Rock+WI");
+        });
+           
+var rockSee = document.getElementById("rockSee");
+rockSee.addEventListener("click", function() {
+            document.getElementById("rock").setAttribute("src", "https://www.google.com/maps/embed/v1/search?key=AIzaSyDGCPoH-xwhnwXsd7kMVchttZMdFIIyiu8&q=The+House+on+the+Rock+WI");
+        });
+
+var rockHotels = document.getElementById("rockHotels");
+rockHotels.addEventListener("click", function() {
+            document.getElementById("rock").setAttribute("src", "https://www.google.com/maps/embed/v1/search?key=AIzaSyDGCPoH-xwhnwXsd7kMVchttZMdFIIyiu8&q=hotels+near+The+House+on+the+Rock+WI");
+        });
+
+var PeruDine = document.getElementById("PeruDine");
+ PeruDine.addEventListener("click", function() {
+     document.getElementById("Peru").setAttribute("src", "https://www.google.com/maps/embed/v1/search?key=AIzaSyDGCPoH-xwhnwXsd7kMVchttZMdFIIyiu8&q=restaurants+in+Peru+Illinois");
+        });
+           
+var PeruSee = document.getElementById("PeruSee");
+PeruSee.addEventListener("click", function() {
+            document.getElementById("Peru").setAttribute("src", "https://www.google.com/maps/embed/v1/search?key=AIzaSyDGCPoH-xwhnwXsd7kMVchttZMdFIIyiu8&q=tourist+attractions+in+Peru+IL");
+        });
+
+var PeruHotels = document.getElementById("PeruHotels");
+PeruHotels.addEventListener("click", function() {
+            document.getElementById("Peru").setAttribute("src", "https://www.google.com/maps/embed/v1/search?key=AIzaSyDGCPoH-xwhnwXsd7kMVchttZMdFIIyiu8&q=hotels+in+Peru+IL");
+        });
+
+var ElPasoDine = document.getElementById("ElPasoDine");
+ ElPasoDine.addEventListener("click", function() {
+     document.getElementById("ElPaso").setAttribute("src", "https://www.google.com/maps/embed/v1/search?key=AIzaSyDGCPoH-xwhnwXsd7kMVchttZMdFIIyiu8&q=restaurants+in+El+Paso+Illinois");
+        });
+           
+var ElPasoSee = document.getElementById("ElPasoSee");
+ElPasoSee.addEventListener("click", function() {
+            document.getElementById("ElPaso").setAttribute("src", "https://www.google.com/maps/embed/v1/search?key=AIzaSyDGCPoH-xwhnwXsd7kMVchttZMdFIIyiu8&q=tourist+attractions+in+El+Paso+IL");
+        });
+
+var ElPasoHotels = document.getElementById("ElPasoHotels");
+ElPasoHotels.addEventListener("click", function() {
+            document.getElementById("ElPaso").setAttribute("src", "https://www.google.com/maps/embed/v1/search?key=AIzaSyDGCPoH-xwhnwXsd7kMVchttZMdFIIyiu8&q=hotels+in+El+Paso+IL");
+        });
+
+var ChesterDine = document.getElementById("ChesterDine");
+ ChesterDine.addEventListener("click", function() {
+     document.getElementById("Chester").setAttribute("src", "https://www.google.com/maps/embed/v1/search?key=AIzaSyDGCPoH-xwhnwXsd7kMVchttZMdFIIyiu8&q=restaurants+in+Chester+Illinois");
+        });
+           
+var ChesterSee = document.getElementById("ChesterSee");
+ChesterSee.addEventListener("click", function() {
+            document.getElementById("Chester").setAttribute("src", "https://www.google.com/maps/embed/v1/search?key=AIzaSyDGCPoH-xwhnwXsd7kMVchttZMdFIIyiu8&q=tourist+attractions+in+Chester+IL");
+        });
+
+var ChesterHotels = document.getElementById("ChesterHotels");
+ChesterHotels.addEventListener("click", function() {
+            document.getElementById("Chester").setAttribute("src", "https://www.google.com/maps/embed/v1/search?key=AIzaSyDGCPoH-xwhnwXsd7kMVchttZMdFIIyiu8&q=hotels+in+Chester+IL");
+        });
+
 
 
