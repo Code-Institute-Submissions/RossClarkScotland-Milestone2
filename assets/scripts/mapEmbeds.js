bbaccebdf5eba413faf91c516c590c92a20690ed
RossClarkScotland <rossclarkscotland@gmail.com>
2020-09-06 15:23:28 +0000

    Added Eagle Point, Chicago, and Madison, WI sections to Trip page

--- assets/scripts/mapEmbeds.js
+++ assets/scripts/mapEmbeds.js
@@ -3,8 +3,8 @@
      document.getElementById("StLouis").setAttribute("src", "https://www.google.com/maps/embed/v1/search?key=AIzaSyDGCPoH-xwhnwXsd7kMVchttZMdFIIyiu8&q=restaurants+in+St+Louis");
         });
            
-var SLDSee = document.getElementById("SLDSee");
-SLDSee.addEventListener("click", function() {
+var SLDSee = document.getElementById("SLSee");
+SLSee.addEventListener("click", function() {
             document.getElementById("StLouis").setAttribute("src", "https://www.google.com/maps/embed/v1/search?key=AIzaSyDGCPoH-xwhnwXsd7kMVchttZMdFIIyiu8&q=tourist+attractions+in+St+Louis");
         });
 
@@ -12,5 +12,51 @@ var SLHotels = document.getElementById("SLHotels");
 SLHotels.addEventListener("click", function() {
             document.getElementById("StLouis").setAttribute("src", "https://www.google.com/maps/embed/v1/search?key=AIzaSyDGCPoH-xwhnwXsd7kMVchttZMdFIIyiu8&q=hotels+in+St+Louis");
         });
+
+ var EPDine = document.getElementById("EPDine");
+ EPDine.addEventListener("click", function() {
+     document.getElementById("EaglePoint").setAttribute("src", "https://www.google.com/maps/embed/v1/search?key=AIzaSyDGCPoH-xwhnwXsd7kMVchttZMdFIIyiu8&q=restaurants+in+Eagle+Point+IN");
+        });
+           
+var EPSee = document.getElementById("EPSee");
+EPSee.addEventListener("click", function() {
+            document.getElementById("EaglePoint").setAttribute("src", "https://www.google.com/maps/embed/v1/search?key=AIzaSyDGCPoH-xwhnwXsd7kMVchttZMdFIIyiu8&q=tourist+attractions+in+Eagle+Point+IN");
+        });
+
+var EPHotels = document.getElementById("EPHotels");
+EPHotels.addEventListener("click", function() {
+            document.getElementById("EaglePoint").setAttribute("src", "https://www.google.com/maps/embed/v1/search?key=AIzaSyDGCPoH-xwhnwXsd7kMVchttZMdFIIyiu8&q=hotels+in+Eagle+Point+IN");
+        });
+
+ var ChiDine = document.getElementById("ChiDine");
+ ChiDine.addEventListener("click", function() {
+     document.getElementById("Chicago").setAttribute("src", "https://www.google.com/maps/embed/v1/search?key=AIzaSyDGCPoH-xwhnwXsd7kMVchttZMdFIIyiu8&q=restaurants+in+Chicago+IL");
+        });
+           
+var ChiSee = document.getElementById("ChiSee");
+ChiSee.addEventListener("click", function() {
+            document.getElementById("Chicago").setAttribute("src", "https://www.google.com/maps/embed/v1/search?key=AIzaSyDGCPoH-xwhnwXsd7kMVchttZMdFIIyiu8&q=tourist+attractions+in+Chicago+IL");
+        });
+
+var ChiHotels = document.getElementById("ChiHotels");
+ChiHotels.addEventListener("click", function() {
+            document.getElementById("Chicago").setAttribute("src", "https://www.google.com/maps/embed/v1/search?key=AIzaSyDGCPoH-xwhnwXsd7kMVchttZMdFIIyiu8&q=hotels+in+Chicago+IL");
+        });
+
+ var MadWIDine = document.getElementById("MadWIDine");
+ MadWIDine.addEventListener("click", function() {
+     document.getElementById("MadWI").setAttribute("src", "https://www.google.com/maps/embed/v1/search?key=AIzaSyDGCPoH-xwhnwXsd7kMVchttZMdFIIyiu8&q=restaurants+in+Madison+WI");
+        });
+           
+var MadWISee = document.getElementById("MadWISee");
+MadWISee.addEventListener("click", function() {
+            document.getElementById("MadWI").setAttribute("src", "https://www.google.com/maps/embed/v1/search?key=AIzaSyDGCPoH-xwhnwXsd7kMVchttZMdFIIyiu8&q=tourist+attractions+in+Madison+WI");
+        });
+
+var MadWIHotels = document.getElementById("MadWIHotels");
+MadWIHotels.addEventListener("click", function() {
+            document.getElementById("MadWI").setAttribute("src", "https://www.google.com/maps/embed/v1/search?key=AIzaSyDGCPoH-xwhnwXsd7kMVchttZMdFIIyiu8&q=hotels+in+Madison+WI");
+        });
+
 
 
