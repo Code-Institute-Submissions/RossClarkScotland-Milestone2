f9160f4e4ffb93f028a26c987d236076e7f9e1f6
RossClarkScotland <rossclarkscotland@gmail.com>
2020-09-20 18:29:07 +0000

    Added constructor functions for players and opponents in the Game

---
+++ assets/scripts/opponent.js
@@ -0,0 +1,11 @@
+const opponent;
+
+function Opponent(opponentType, health, strength, speed, intelligence, agility, endurance) {
+    this.classType = opponentType;
+    this.health = health;
+    this.strength = strength;
+    this.speed = speed;
+    this.intelligence = intelligence;
+    this.agility = agility;
+    this.endurance = endurance;
+}
---
+++ assets/scripts/player.js
@@ -0,0 +1,11 @@
+const player;
+
+function Player(classType, health, strength, speed, intelligence, agility, endurance) {
+    this.classType = classType;
+    this.health = health;
+    this.strength = strength;
+    this.speed = speed;
+    this.intelligence = intelligence;
+    this.agility = agility;
+    this.endurance = endurance;
+}
