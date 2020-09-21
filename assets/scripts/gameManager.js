6204b4da32628a8f524a7ae7207286c64da653e0
RossClarkScotland <rossclarkscotland@gmail.com>
2020-09-21 13:03:10 +0000

    Added switch statement for player choice in gameManager.js

--- assets/scripts/gameManager.js
+++ assets/scripts/gameManager.js
@@ -1,3 +1,27 @@
 const gameManager = {
-    
+    setGameStart: function(classType) {
+        this.setPlayer(classType);
+        this.setPreFight();
+    },
+
+    setPlayer: function(classType) {
+        switch (classType) {
+            case: "Wednesday":
+                player = new Player(classType, 7, 6, 6, 10, 6, 10);
+                break;
+            case: "Bilquis":
+                player = new Player(classType, 6, 7, 6, 8, 10, 9);
+                break;
+            case: "Nancy":
+                player = new Player(classType, 7, 7, 7, 9, 8, 7);
+                break;
+            case: "Czernobog":
+                player = new Player(classType, 8, 10, 6, 7, 6, 8);
+                break;
+        }
+    },
+
+    setPreFight: function() {
+
+    }
 }
