c4c9450b2183d27a877277cb4c2b48f334b8918b
RossClarkScotland <rossclarkscotland@gmail.com>
2020-09-22 19:16:02 +0000

    Added and debugged setPreFight and setFight functions in gameManager.js

--- assets/scripts/gameManager.js
+++ assets/scripts/gameManager.js
@@ -25,6 +25,23 @@ let gameManager = {
     },
 
     setPreFight: function() {
+        let getHeader = document.querySelector(".game-header");
+        let getActions = document.querySelector(".actions");
+        let getBattleground = document.querySelector(".battleground");
+        getHeader.innerHTML = '<p>Step to the line: Find your opponent!</p>';
+        getActions.innerHTML = '<a href="#" class="btn-prefight" onclick="gameManager.setFight()">Search!</a>';
+        getBattleground.style.visibility = visible;
+    },
 
+    setFight: function() {
+        let getHeader = document.querySelector(".game-header");
+        let getActions = document.querySelector(".actions");
+        let getOpponent = document.querySelector(".opponent");
+        let opponent00 = new Opponent("Technical Boy", 6, 8, 6, 9, 6, 10);
+        let opponent01 = new Opponent("Media", 6, 7, 9, 9, 8, 6);
+        let opponent03 = new Opponent("Mister World", 7, 8, 8, 8, 7, 7);
+        let opponent04 = new Opponent("Spooks", 7, 7, 7, 7, 7, 7);
+        let chooseOpponent = Math.floor(Math.random() * Math.floor(4) + 1);
+        console.log(chooseOpponent);
     }
 };
