d06f7507f2872d01b9eb5929b0e58ee350aab654
RossClarkScotland <rossclarkscotland@gmail.com>
2020-10-31 09:51:14 +0000

    Added semi-colons

--- assets/scripts/sendEmail.js
+++ assets/scripts/sendEmail.js
@@ -7,14 +7,14 @@ function sendMail(contactForm) {
     .then(
         function(response) {
             console.log("success", response);
-            document.querySelector("#contact-header").innerHTML = "Your form has been sent!"
-            document.querySelector("#contact-sub").innerHTML = "May the gods hear your call!"
+            document.querySelector("#contact-header").innerHTML = "Your form has been sent!";
+            document.querySelector("#contact-sub").innerHTML = "May the gods hear your call!";
             
         },
         function(error) {
             console.log("failed", error);
-            document.querySelector("#contact-header").innerHTML = "There was a problem with your form submission."
-            document.querySelector("#contact-sub").innerHTML = "Please try again later."
+            document.querySelector("#contact-header").innerHTML = "There was a problem with your form submission.";
+            document.querySelector("#contact-sub").innerHTML = "Please try again later.";
         }
     );
         return false;
